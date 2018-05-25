0560b08f0ffad48cd89c17dea10d12dde655f922
Matheus Bandeira <matheusbandeira944@gmail.com>
2018-05-25 11:36:23 -0300

    ajustes de código e retirando comentários

--- src/main/java/com/desafio/api/b2w/ApiDesafioB2WApplication.java
+++ src/main/java/com/desafio/api/b2w/ApiDesafioB2WApplication.java
@@ -15,28 +15,9 @@ import java.util.Set;
 @SpringBootApplication
 public class ApiDesafioB2WApplication {
 
-	// public static final String API_URL = "https://swapi.co/api/planets/1/";
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiDesafioB2WApplication.class, args);
 
 	}
-//
-//	 @Override
-//	 public void run(String... arg0) throws Exception {
-//	 RestTemplate restTemplate = new RestTemplate();
-//	 HttpHeaders headers = new HttpHeaders();
-//	 headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
-//	 headers.add("user-agent", "Chrome/54.0.2840.99");
-//	 HttpEntity<?> entity = new HttpEntity<>(headers);
-//	 HttpEntity<Planet> response = restTemplate.exchange(
-//	 API_URL,
-//	 HttpMethod.GET,
-//	 entity,
-//	 Planet.class);
-//	 Planet planet = response.getBody();
-//	 System.out.println(planet);
-//	
-//	 }
 
 }
